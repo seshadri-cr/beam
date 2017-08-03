aba18ff69ae50ae393c9564c75df1c7588545c6a
Seshadri Chakkravarthy <sesh.cr@gmail.com>
2017-08-03 00:56:02 -0700

    Initial draft commit for MemcachedIO

---
+++ sdks/java/io/memcached/src/main/java/org/apache/beam/sdk/io/memcached/MemcachedIO.java
@@ -0,0 +1,218 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.beam.sdk.io.memcached;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import com.google.auto.value.AutoValue;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.spy.memcached.AddrUtil;
+import net.spy.memcached.ConnectionFactoryBuilder;
+import net.spy.memcached.ConnectionFactoryBuilder.Protocol;
+import net.spy.memcached.MemcachedClient;
+
+import org.apache.beam.sdk.annotations.Experimental;
+import org.apache.beam.sdk.options.PipelineOptions;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.display.DisplayData;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PDone;
+
+/**
+ * IO to read and write data on Memcached key-store.
+ *
+ */
+@Experimental
+public class MemcachedIO {
+  /**
+   * Read data from a JDBC datasource.
+   *
+   */
+  public static Read read() {
+    return new AutoValue_MemcachedIO_Read.Builder().build();
+  }
+
+  public static Write write() {
+    return new AutoValue_MemcachedIO_Write.Builder().build();
+  }
+
+
+  private MemcachedIO() {}
+
+  /** A {@link PTransform} to read data from a memcached key-value store. */
+  @AutoValue
+  public abstract static class Read extends
+  PTransform<PCollection<String>, PCollection<KV<String, Object>>> {
+    @Nullable abstract Protocol getProtocol();
+    @Nullable abstract List<String> getAddresses();
+
+    abstract Builder toBuilder();
+
+    @AutoValue.Builder
+    abstract static class Builder {
+      abstract Builder setProtocol(Protocol protocol);
+      abstract Builder setAddresses(List<String> addresses);
+      abstract Read build();
+    }
+
+    public Read withProtocol(Protocol protocol) {
+      checkArgument(protocol != null,
+          "MemcachedIO.read().withProtocol(protocol) called with null protocol");
+      return toBuilder().setProtocol(protocol).build();
+    }
+
+    public Read withAddresses(List<String> addresses) {
+      checkArgument(addresses != null,
+          "MemcachedIO.read().withAddresses(addresses) called with null addresses");
+      return toBuilder().setAddresses(addresses).build();
+    }
+
+    @Override
+    public PCollection<KV<String, Object>> expand(PCollection<String> input) {
+      return input
+          .apply(ParDo.of(new ReadFn(this)));
+    }
+
+    @Override
+    public void validate(PipelineOptions options) {
+    }
+
+    @Override
+    public void populateDisplayData(DisplayData.Builder builder) {
+      super.populateDisplayData(builder);
+/*      builder.add(DisplayData.item("query", getQuery()));
+      builder.add(DisplayData.item("rowMapper", getRowMapper().getClass().getName()));
+      builder.add(DisplayData.item("coder", getCoder().getClass().getName()));
+*/    }
+
+    /** A {@link DoFn} executing the SQL query to read from the database. */
+    static class ReadFn extends DoFn<String, KV<String, Object>> {
+      private static final int DEFAULT_BATCH_SIZE = 1;
+      private MemcachedIO.Read spec;
+      private MemcachedClient memcachedClient;
+
+      private ReadFn(Read spec) {
+        this.spec = spec;
+      }
+
+      @Setup
+      public void setup() throws Exception {
+        memcachedClient = new MemcachedClient(
+            new ConnectionFactoryBuilder()
+            .setProtocol(spec.getProtocol())
+            .build(),
+            AddrUtil.getAddresses(spec.getAddresses()));
+      }
+
+      @ProcessElement
+      public void processElement(ProcessContext context) throws Exception {
+        String key = context.element();
+        Object value = memcachedClient.get(key);
+        context.output(KV.of(key, value));
+      }
+
+      @Teardown
+      public void teardown() throws Exception {
+        memcachedClient.shutdown();
+      }
+    }
+  }
+
+  /** A {@link PTransform} to read data from a memcached key-value store. */
+  @AutoValue
+  public abstract static class Write extends PTransform< PCollection<KV<String, Object>>, PDone> {
+    @Nullable abstract Protocol getProtocol();
+    @Nullable abstract List<String> getAddresses();
+
+    abstract Builder toBuilder();
+
+    @AutoValue.Builder
+    abstract static class Builder {
+      abstract Builder setProtocol(Protocol protocol);
+      abstract Builder setAddresses(List<String> addresses);
+      abstract MemcachedIO.Write build();
+    }
+
+    public Write withProtocol(Protocol protocol) {
+      checkArgument(protocol != null,
+          "MemcachedIO.read().withProtocol(protocol) called with null protocol");
+      return toBuilder().setProtocol(protocol).build();
+    }
+
+    public Write withAddresses(List<String> addresses) {
+      checkArgument(addresses != null,
+          "MemcachedIO.read().withAddresses(addresses) called with null addresses");
+      return toBuilder().setAddresses(addresses).build();
+    }
+
+    @Override
+    public PDone expand(PCollection<KV<String, Object>> input) {
+      input.apply(ParDo.of(new WriteFn(this)));
+      return PDone.in(input.getPipeline());
+    }
+
+    @Override
+    public void validate(PipelineOptions options) {
+    }
+
+    @Override
+    public void populateDisplayData(DisplayData.Builder builder) {
+      super.populateDisplayData(builder);
+/*      builder.add(DisplayData.item("query", getQuery()));
+      builder.add(DisplayData.item("rowMapper", getRowMapper().getClass().getName()));
+      builder.add(DisplayData.item("coder", getCoder().getClass().getName()));
+*/    }
+
+    /** A {@link DoFn} executing the SQL query to read from the database. */
+    static class WriteFn extends DoFn<KV<String, Object>, Void> {
+      private static final int DEFAULT_BATCH_SIZE = 1;
+      private MemcachedIO.Write spec;
+      private MemcachedClient memcachedClient;
+
+      private WriteFn(Write spec) {
+        this.spec = spec;
+      }
+
+      @Setup
+      public void setup() throws Exception {
+        memcachedClient = new MemcachedClient(
+            new ConnectionFactoryBuilder()
+            .setProtocol(spec.getProtocol())
+            .build(),
+            AddrUtil.getAddresses(spec.getAddresses()));
+      }
+
+      @ProcessElement
+      public void processElement(ProcessContext context) throws Exception {
+        memcachedClient.set(context.element().getKey(), 3600, context.element().getValue());
+      }
+
+      @Teardown
+      public void teardown() throws Exception {
+        memcachedClient.shutdown();
+      }
+    }
+  }
+
+}
